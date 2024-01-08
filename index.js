395c2cb3a227659940364c9adc6719a86e897ff4
Hunter <Huntermatyi@gmail.com>
2024-01-08 15:31:27 -0500

    donezo

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+
+let multiply = num1 * num2; 
+
+multiply; 
+
+let random = Math.floor(Math.random() * 10) + 1;
+random;
+
+
+let num3 = 12;
+let num4 = 3;
+let mod = Math.round(num3 / num4);
+
+mod;
+
+let max = Math.max(1, 2, 5, 20);
+max;
